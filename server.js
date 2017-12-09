a4878adfe908b88b59d453938343ebb96e177035
Nathalie Christmann-Cooper <nathalie@shecodes.co.uk>
2017-12-09 00:18:35 +0000

    Line 4 tells express to use ejs. Lines 5 to 9 defines a list of tasks for future use. Added a javascript loop to cause ejs to create a separate text field for each of the tasks in the task array.The text fields are given a value from the task array via a value attribute set within ejs tags.

--- server.js
+++ server.js
@@ -1,4 +1,10 @@
 var express = require ('express');
 var app = express();
 app.use(express.static('views'));
+app.set('view engine', 'ejs');
+app.all('/', function(req, res){
+  res.render('index', {
+    tasks:['Create Node.JS App', 'Have whiskey', '']
+  });
+});
 app.listen(1337);
