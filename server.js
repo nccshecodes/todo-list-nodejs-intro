0f513b0cf3f43997bae4587ca389b93e0dff492e
Nathalie Christmann-Cooper <nathalie@shecodes.co.uk>
2017-12-09 09:24:03 +0000

    Added body-parser package to handle form data.

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 var express = require ('express');
+var bodyParser = require('body-parser');
 var app = express();
+var tasks = ['Create Node.JS App', 'Have whiskey'];
 app.use(express.static('views'));
+app.use(bodyParser.urlencoded({extended:true}));
 app.set('view engine', 'ejs');
-app.all('/', function(req, res){
+app.all('/', function(req, res) {
+  if(req.body.tasks) {
+    tasks = req.body.tasks.filter(function(val) {return val;});
+  }
+  tasks.push('');
   res.render('index', {
-    tasks:['Create Node.JS App', 'Have whiskey', '']
+    tasks: tasks
   });
 });
 app.listen(1337);
